fdb7201d3928d20e42ea11c3c48878d45cf419b3
abhirama951 <abhirama2603@gmail.com>
2025-10-26 11:55:36 +0000

    Test commit

--- git.vhdl
+++ git.vhdl
@@ -4,6 +4,6 @@ use ieee.std_logic_1164.all;
 library work;
 
 package git is
-    constant GIT_HASH : std_ulogic_vector(55 downto 0) := x"407477905109de";
+    constant GIT_HASH : std_ulogic_vector(55 downto 0) := x"684a776274c0c2";
     constant GIT_DIRTY : std_ulogic := '1';
 end git;
